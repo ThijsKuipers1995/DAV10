bb9e25c9d81defec6f607f10924407bfc3e78304
Tijmen van Etten <tijmenettenvan@gmail.com>
2018-06-24 14:46:20 +0200

    Moved external files to seperate folder

---
+++ website/files/scripts.js
@@ -0,0 +1,94 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+var info = document.getElementById("Info");
+var graphs = document.getElementById("Graphs");
+var conflicts = document.getElementById("Conflict");
+
+function showInfo() {
+	modal.style.display = "none";
+
+    info.style.display= "block";
+	conflicts.style.display = "none";
+    graphs.style.display = "none";
+}
+
+function showAllGraphs() {
+	modal.style.display = "none";
+
+	info.style.display= "none";
+	conflicts.style.display = "none";
+	graphs.style.display = "block";
+}
+
+function showConflicts() {
+	modal.style.display = "none";
+
+	info.style.display= "none";
+	conflicts.style.display = "block";
+	graphs.style.display = "none";
+}
+
+function showGraphs(country) {
+	modal.style.display = "none";
+	alert('Hello '+country+'!')
+}
+(function() {
+            var fn = function() {
+              Bokeh.safely(function() {
+                (function(root) {
+                  function embed_document(root) {
+                    
+                  var docs_json = document.getElementById('b174875b-d62b-4bd6-bdb0-0c0446dc1c00').textContent;
+                  var render_items = [{"docid":"64032834-179b-4b25-b017-4a7b0760f900","elementid":"a407a123-f539-4190-ad1c-3f6e4e4b206e","modelid":"d6af7d9b-0c25-4938-9ca3-ce292aeda121"}];
+                  root.Bokeh.embed.embed_items(docs_json, render_items);
+                
+                  }
+                  if (root.Bokeh !== undefined) {
+                    embed_document(root);
+                  } else {
+                    var attempts = 0;
+                    var timer = setInterval(function(root) {
+                      if (root.Bokeh !== undefined) {
+                        embed_document(root);
+                        clearInterval(timer);
+                      }
+                      attempts++;
+                      if (attempts > 100) {
+                        console.log("Bokeh: ERROR: Unable to run BokehJS code because BokehJS library is missing")
+                        clearInterval(timer);
+                      }
+                    }, 10, root)
+                  }
+                })(window);
+              });
+            };
+            if (document.readyState != "loading") fn();
+            else document.addEventListener("DOMContentLoaded", fn);
+          })();
+
+
+
+
